5899927008eaf75531b8c89342371c30e843e6ea
hefen <hsun35@jhu.edu>
2018-02-22 11:55:22 -0600

    Initial commit

---
+++ app/src/main/java/com/example/project_walgreens/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.example.project_walgreens;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import static java.lang.Thread.sleep;
+
+/**
+ * Created by hefen on 2/22/2018.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Start home activity
+        Thread launcher = new Thread(new SplashScreenLauncher());
+        launcher.start();
+
+        //startActivity(new Intent(SplashActivity.this, MainActivity.class));
+
+        // close splash activity
+        //finish();
+    }
+    private class SplashScreenLauncher implements Runnable {
+        public void run() {
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Intent i = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(i);
+            SplashActivity.this.finish();
+        }
+    }
+}
